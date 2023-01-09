bba77ce59c4f525ab85d3e4b7c21730c7c6ede4a
ARKA1112 <bagha92@gmail.com>
2023-01-09 18:03:03 +0530

    .

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,9 @@
 #Home page route
-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Arka'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-        <a href="http://www.google.in">Google_in</a>
-    </body>
-</html>'''
+    return render_template('index.html',title='Home',user=user)
