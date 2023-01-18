5d4bd3b902d0aad1b3a5fc55e1543dc548a80623
ARKA1112 <bagha92@gmail.com>
2023-01-18 23:40:03 +0530

    .

--- microblog.py
+++ microblog.py
@@ -1,8 +1,8 @@
 from app import app
-from app.models import User,Post
+from app.models import User,Post,db
 
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db':db, 'User':User, 'Post':Post}u = 
+    return {'db':db, 'User':User, 'Post':Post}
